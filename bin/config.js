bb23ca815c6026fb32c03d8727b8f492b8ff69fe
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-07-10 12:30:55 -0500

    Delete bin directory

--- bin/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-const conf = (0, dotenv_1.config)().parsed;
-exports.default = conf || {};
